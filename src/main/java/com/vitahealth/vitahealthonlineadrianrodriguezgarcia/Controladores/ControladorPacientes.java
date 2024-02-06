261d1a7e03556c4cf04a78db9a2345c2f2dc197f
Dctr-LurS <adrianrg2004@gmail.com>
2024-02-06 23:48:51 +0100

    Implementacion de controlador y actualizacion en la configuracion

---
+++ src/main/java/com/vitahealth/vitahealthonlineadrianrodriguezgarcia/Controladores/ControladorPacientes.java
@@ -0,0 +1,13 @@
+package com.vitahealth.vitahealthonlineadrianrodriguezgarcia.Controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControladorPacientes {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
--- src/main/webapp/WEB-INF/Vistas/index.jsp
+++ src/main/webapp/WEB-INF/Vistas/index.jsp
@@ -2,12 +2,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>JSP - Hello World</title>
+    <title>VitaHealth - Online</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
+<h1><%= "Hello World!" %></h1>
 <br/>
-<a href="hello-servlet">Hello Servlet</a>
 </body>
 </html>
